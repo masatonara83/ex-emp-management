c842aab37a642beae35c044d5a0a86c620414858
masatonara83 <masato.nara@rakus-partners.co.jp>
2021-11-02 14:14:12 +0900

    Adminstractor（domain）完成

---
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -0,0 +1,73 @@
+package jp.co.sample.domain;
+
+/**
+ * adominstratorのドメイン
+ * 
+ * @author naramasato
+ *
+ */
+public class Administrator {
+
+	//id
+	private Integer id;
+	//名前
+	private String name;
+	//メールアドレス
+	private String mailAddress;
+	//パスワード
+	private String password;
+	
+	//コンストラクター
+	public Administrator(Integer id, String name, String mailAddress, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mailAddress = mailAddress;
+		this.password = password;
+	}
+	
+	//ゲッターとセッター
+	public Administrator() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	//toStringの記載
+	@Override
+	public String toString() {
+		return "Administrator [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+				+ "]";
+	}
+	
+	
+}
